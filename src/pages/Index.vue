511a58d5c79544d7b9a4a7a1256dc83649236b59
csedgwick <sedgwick@live.com.au>
2021-03-31 14:08:03 -0600

    commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+  </q-page>
+</template>
+
+<script>
+const sharp = require("sharp");
+export default {
+  name: "PageIndex"
+};
+</script>
